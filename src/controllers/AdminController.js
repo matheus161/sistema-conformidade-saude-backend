087418c68086272dd1159b7592b2c814d108bbd6
Matheus <matheeus161@gmail.com>
2022-05-01 16:55:44 -0300

    fix User

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -66,7 +66,6 @@ async function update(req, res) {
 
         return res.status(200).json(adminUpdate);
     } catch (error) {
-        console.log(error);
         return res.status(500).json({ message: 'Internal server error' });
     }
 }
