0a9ad5c03649e6aed10bdba03ae4463c53335332
Matheus <matheeus161@gmail.com>
2022-05-11 13:29:27 -0300

    fix: console.log

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -109,7 +109,6 @@ async function resetPassword(req, res) {
         return res.status(200).json({ message: 'Email has been sent'});
 
     } catch (error) {
-        console.log(error);
         return res.status(500).json({ message: 'Internal server error' });
     }
 }
